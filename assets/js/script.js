942bac663c09ba4c04009cef0a0be5e0effc7fec
Barby87 <bcaceres.sepulveda@hotmail.com>
2020-01-26 01:51:32 -0300

    Probando formulario

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,27 +13,6 @@ typing();
 
 
 $(document).ready(function() {
-    //     //Navbar
-    //     $(window).scroll(function() {
-    //             if ($(window).scrollTop() > 300) {
-    //                 $('nav').addClass('grey darken-4');
-    //             } else {
-    //                 $('nav').removeClass('grey darken-4');
-    //             }
-    //         })
-    //     // Floating Action Button
-    //     $('.fixed-action-btn').floatingActionButton();
 
-    //     // Tooltip button
-    //     $('.tooltipped').tooltip();
 
-    // Aniview
-    $('.aniview').AniView();
-    //Options
-    var options = {
-        animateThreshold: 100,
-        scrollPollInterval: 20
-    }
-
-    $('.aniview').AniView(options);
 });
