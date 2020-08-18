5e589a83f54517838823277318c52f00e40400df
Barby87 <bcaceres.sepulveda@hotmail.com>
2020-08-17 21:12:18 -0400

    Adding scrollflow

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,16 @@
 let i = 0;
 let text = "Hola, mi nombre es Bárbara...";
 
-function typing() {
+
+
+setTimeout(function typing() {
     if (i < text.length) {
         document.getElementById('text').innerHTML += text.charAt(i);
         i++;
         setTimeout(typing, 90);
     }
-}
-typing();
+}, 2500);
+
 
 
 $(function () {
